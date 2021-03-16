391cd7a93eaa17b7817cbba4aa457eadb1395238
Nikita Ushakov <ushakovn@tcd.ie>
2021-03-16 11:44:52 +0400

    ui test

--- Assets/Scripts/UI/Camera/CameraAligner.cs
+++ Assets/Scripts/UI/Camera/CameraAligner.cs
@@ -2,25 +2,25 @@
 
 public class CameraAligner : MonoBehaviour
 {
-    public float multiplier = 0.000002f;
-    public float maxScale = 0.01f;
-    private float fieldOfView;
+    public float scaleFactor;
+    public float minScale;
+    public float maxScale;
     
     private Transform worldTransform;
     private Transform cameraTransform;
-    
+
     private void Start()
     {
         worldTransform = transform;
         var cam = Manager.mainCamera;
         cameraTransform = cam.transform;
-        fieldOfView = cam.fieldOfView;
+        scaleFactor = cam.fieldOfView;
     }
 
     public void Update()
     {
         var direction = worldTransform.position - cameraTransform.position;;
-        var scale = Mathf.Clamp(direction.magnitude * multiplier * fieldOfView, 0f, maxScale);
+        var scale = Mathf.Clamp(direction.magnitude / scaleFactor, minScale, maxScale);
         worldTransform.localScale = new Vector3(scale, scale, scale);
         worldTransform.forward = direction;
     }
