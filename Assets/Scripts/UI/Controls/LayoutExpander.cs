e1fdaf0da604d19a86a66468ce32ae575ca194d1
Nikita Ushakov <ushakovn@tcd.ie>
2021-03-19 11:01:12 +0400

    Before rework

--- Assets/Scripts/UI/Controls/LayoutExpander.cs
+++ Assets/Scripts/UI/Controls/LayoutExpander.cs
@@ -1,24 +1,40 @@
+using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class LayoutExpander : UIBehaviour
 {
-    public float extraWidth;
-    public RectTransform rectLayout;
-
-    private float height;
+    public Rectangle[] rectangles;
     private RectTransform rectTransform;
 
+    [Serializable]
+    public class Rectangle
+    {
+        public RectTransform transform;
+        public float minWidth;
+    }
+    
     protected override void Awake()
     {
         rectTransform = GetComponent<RectTransform>();
-        height = rectLayout.sizeDelta.y;
     }
 
     protected override void OnRectTransformDimensionsChange()
     {
         if (rectTransform) {
-            rectLayout.sizeDelta = new Vector2(rectTransform.sizeDelta.x + extraWidth, height);
+            var max = rectTransform.sizeDelta.x;
+            foreach (var rect in rectangles) {
+                var trans = rect.transform;
+                var width = Mathf.Max(rect.minWidth, max);
+                if (width <= 0f) {
+                    trans.gameObject.SetActive(false);
+                } else {
+                    trans.gameObject.SetActive(true);
+                    var sizeDelta = trans.sizeDelta;
+                    sizeDelta.x = width;
+                    trans.sizeDelta = sizeDelta;
+                }
+            }
         }
     }
 }
